da2bb0e6d8efd4d45c39f8c5c58770312df9bd0e
RohanKapurDEV <rohankapurdev@outlook.com>
2019-01-11 02:17:06 -0500

    SF-Pro text typeface import and global definitions

--- src/app/services/wallet-gen.service.ts
+++ src/app/services/wallet-gen.service.ts
@@ -6,7 +6,7 @@ import * as bitcoin from 'bitcoinjs-lib';
  * The walletGen Service is to allow the creation and import of several different types of wallets.
  * Current support is for Bitcoin and Ethereum. The service also integrates 2 storage mechanisms, one persistant
  * and the other, persistant and highly secure. When returning wallet data from generation of wallet, use SQLite
- * storage for public keys and SecureStorage for private keys.
+ * storage for public keys with relevant metadata and SecureStorage for respective private keys.
  * 
  * FORGET ABOUT THIS UNTIL THE DEVROOM IS COMPLETELY FULL AND YOU KNOW EXACTLY WHAT THE FUCK IS GOING ON
  * 
@@ -32,7 +32,7 @@ export class WalletGenService {
     let input = this.mnemonic;
     let wallet = ethers.Wallet.fromMnemonic(input);
     this.mnemonic = ''; //Wiping cached variable for security
-    return wallet; // Replace return statements with those that isolate components of the return type for usage in storage mechanisms (e.g secureStore(wallet.privateKey) or maybe a SQL statemnt (STORE _id, pk, 'ethereum', 'date/time created') something idk)
+    return wallet; // Replace return statements with those that isolate components of the return type for usage in storage mechanisms (e.g secureStore(wallet.privateKey) or maybe an SQL statemnt (STORE _id, pk, 'ethereum', 'date/time created') something idk)
   }
 
   generateEtherWalletFromPrivateKey(): Wallet {
